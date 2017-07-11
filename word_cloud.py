9dcba25705cbe3aa3ff4773e49520257ce47242f
Olivia Bettaglio <olivia.bettaglio@gmail.com>
2017-07-11 16:18:05 -0700

    Added Tf-Idf vectorizer.

--- word_cloud.py
+++ word_cloud.py
@@ -1,11 +1,24 @@
 from load_data import corpus
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+tf = TfidfVectorizer(analyzer='word', max_features=100, min_df = 0, ngram_range=(1,3), stop_words = 'english')
 
-cv = CountVectorizer(analyzer='word', max_features=200, min_df=0, stop_words="english")
-counts = cv.fit_transform(corpus).toarray().ravel()
-words = np.array(cv.get_feature_names())
+tfidf_matrix =  tf.fit_transform(corpus)
+feature_names = tf.get_feature_names()
 
-# normalize
-counts = counts / float(counts.max())
+dense = tfidf_matrix.todense()
+
+phrases_by_comment = []
+
+for i in range(0, len(dense)):
+    comment = dense[i].tolist()[0]
+    phrase_scores = [pair for pair in zip(range(0, len(comment)), comment) if pair[1] > 0]
+    phrases_by_comment.append(phrase_scores)
+
+print phrases_by_comment
+
+## output: [[(68, 0.7016522097362441), (95, 0.7125195973250462)],   < first comment
+##          [(56, 1.0)],                                            < second comment...
+##          [],
+##          [(2, 0.5627634875214949), (18, 0.4863293890204364), (50, 0.40534376325697624), (80, 0.5314860450438239)],
+##          ...]
